4285135cff1dda13c9fe8075a051048c8c38c158
blueyellowpink <andrew@trasua.dev>
2023-09-28 19:58:00 +0700

    impl IntoIter for MyVec

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::{
     alloc::{self, Layout},
+    mem::{self, ManuallyDrop},
     ops::{Deref, DerefMut},
     ptr::{self, NonNull},
 };
@@ -148,12 +149,91 @@ impl<T> DerefMut for MyVec<T> {
     }
 }
 
+impl<T> IntoIterator for MyVec<T> {
+    type Item = T;
+    type IntoIter = IntoIter<T>;
+    fn into_iter(self) -> IntoIter<T> {
+        // Make sure not to drop Vec since that would free the buffer
+        let vec = ManuallyDrop::new(self);
+
+        // Can't destructure Vec since it's Drop
+        let ptr = vec.ptr;
+        let cap = vec.cap;
+        let len = vec.len;
+
+        unsafe {
+            IntoIter {
+                buf: ptr,
+                cap,
+                start: ptr.as_ptr(),
+                end: if cap == 0 {
+                    // can't offset off this pointer, it's not allocated!
+                    ptr.as_ptr()
+                } else {
+                    ptr.as_ptr().add(len)
+                },
+            }
+        }
+    }
+}
+
+pub struct IntoIter<T> {
+    buf: NonNull<T>,
+    cap: usize,
+    start: *const T,
+    end: *const T,
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+        if self.start == self.end {
+            return None;
+        }
+        unsafe {
+            let result = ptr::read(self.start);
+            self.start = self.start.offset(1);
+            Some(result)
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = (self.end as usize - self.start as usize) / mem::size_of::<T>();
+        (len, Some(len))
+    }
+}
+
+impl<T> DoubleEndedIterator for IntoIter<T> {
+    fn next_back(&mut self) -> Option<T> {
+        if self.start == self.end {
+            return None;
+        }
+        unsafe {
+            self.end = self.end.offset(-1);
+            Some(ptr::read(self.end))
+        }
+    }
+}
+
+impl<T> Drop for IntoIter<T> {
+    fn drop(&mut self) {
+        if self.cap != 0 {
+            // drop any remaining elements
+            for _ in &mut *self {}
+            let layout = Layout::array::<T>(self.cap).unwrap();
+            unsafe {
+                alloc::dealloc(self.buf.as_ptr() as *mut u8, layout);
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn test_push_pop_insert_remove() {
         let mut vec: MyVec<i32> = MyVec::<i32>::new();
         vec.push(1i32);
         vec.push(2i32);
@@ -175,5 +255,19 @@ mod tests {
         assert_eq!(vec.len(), 4);
         assert_eq!(elem, 1i32);
         assert_eq!(*(vec.get(1).unwrap()), 2i32);
+    }
+
+    #[test]
+    fn test_iter() {
+        let mut vec: MyVec<usize> = MyVec::new();
+        vec.push(1);
+        vec.push(2);
+        vec.push(3);
+
+        let mut iterator = vec.into_iter();
+        assert_eq!(iterator.next(), Some(1));
+        assert_eq!(iterator.next(), Some(2));
+        assert_eq!(iterator.next_back(), Some(3));
+        assert_eq!(iterator.next_back(), None);
     }
 }
